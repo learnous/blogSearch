425a64296845b58aba709e52a50385dad27b13ab
hy-yim <sin_lhy@naver.com>
2023-03-19 02:23:14 +0900

    init project

---
+++ blogSearch-api/src/main/java/com/blogSearch/api/BlogSearchApiApplication.java
@@ -0,0 +1,11 @@
+package com.blogSearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchApiApplication.class, args);
+    }
+}
